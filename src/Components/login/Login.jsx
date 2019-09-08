e5c7f51366484d6098ce633265120f4d5a9f4231
UmmulFatima <hoory1020@gmail.com>
2019-09-09 02:07:13 +0300

    React Login and registration form

---
+++ src/Components/login/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import loginImage from "../../img/Login.svg";
+
+export class Login extends React.Component {
+    // eslint-disable-next-line
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+            <div className="base-container" ref={this.props.containerRef}>
+                <div className="header">Login</div>
+                <div className="content">
+                    <div className="image">
+                        <img src={loginImage} alt="not found"/>
+                    </div>
+                    <div className="form">
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" placeholder="username"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder="password"/>
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <button type="button" className="btn">Login</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
